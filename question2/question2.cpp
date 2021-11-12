82331f322e431d7c5b6a2b573fb7fc72054cc836
Mateus <mateus.ferro.2001@hotmail.com>
2021-11-12 11:04:23 -0300

    DONE - question2

---
+++ question2/question2.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+using namespace std; int main() {int i = 1;  while (i < 101) { if (i % 3 == 0) cout << "Foo" << endl; else if (i % 5 == 0) cout << "Baa" << endl; else if (i % 3 == 0 && i % 5 == 0) cout << "FooBaa" << endl; else cout << i << endl; i++; } return 0; }
