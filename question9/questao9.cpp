5895e46b7425f609b1994441f5c0f495e77145fc
Mateus <mateus.ferro.2001@hotmail.com>
2021-11-13 14:57:19 -0300

    DONE - question9 & question10

---
+++ question9/questao9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "\nLevando em consideracao que o truncamento da tabela vai" << endl;
+    cout << "apagar seus dados (e nao a tabela tab), a partir do momento que" << endl;
+    cout << "for inserido os valores (3,3) e (4,4), estes serao mostrados na" << endl;
+    cout << "selecao de tudo no comando (select count(*))" << endl << endl;
+
+    return 0;
+}
